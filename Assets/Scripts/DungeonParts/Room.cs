c89e12d4544c271c796d27b7ce2106ceef6aaed9
Chilxius <thebennett47@gmail.com>
2026-09-03 14:25:18 -0500

    Room Functional
    Room objects currently create a list of wallSegments that the player cannot push out of the way.

--- Assets/Scripts/DungeonParts/Room.cs
+++ Assets/Scripts/DungeonParts/Room.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public class Room : MonoBehaviour
 {
+    [SerializeField] private WallSegment prefabSegment;
     List<WallSegment> Walls {get; set;}
 
     private void Awake()
@@ -15,7 +16,21 @@ public class Room : MonoBehaviour
     {
         for(int i = 0; i < 15; i++ )
         {
-            Walls.Add( new WallSegment() );
+            WallSegment newSegment = Instantiate(prefabSegment);
+            newSegment.Initialize( new Vector3Int(i-7, -4, 0) );
+            Walls.Add( newSegment );
+            newSegment = Instantiate(prefabSegment);
+            newSegment.Initialize( new Vector3Int(i-7, 4, 0) );
+            Walls.Add( newSegment );
+        }
+        for(int i = 0; i < 7; i++ )
+        {
+            WallSegment newSegment = Instantiate(prefabSegment);
+            newSegment.Initialize( new Vector3Int(-7, i-3, 0) );
+            Walls.Add( newSegment );
+            newSegment = Instantiate(prefabSegment);
+            newSegment.Initialize( new Vector3Int(7, i-3, 0) );
+            Walls.Add( newSegment );
         }
     }
 }
